66b029917b660919e1877ede5636ba2eff3d1f36
longsiyuan <>
2019-03-27 11:02:25 +0800

    vue

---
+++ webpack.config.js
@@ -0,0 +1,45 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const CleanWebpackPlugin = require('clean-webpack-plugin');
+
+module.exports = {
+    entry:{
+        app:'./src/index.js',
+        print:'./src/print.js'
+    },
+    output:{
+        filename:'[name].bundle.js',
+        path:path.resolve(__dirname,'dist')
+    },
+    devtool:'inline-source-map',
+    devServer:{
+        contentBase:'./dist'
+    },
+    plugins:[
+        new CleanWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title:"output management"
+        })
+    ],
+    module:{
+        rules:[
+            {
+                test:/\.css$/,
+                use:[
+                    'style-loader',
+                    'css-loader'
+                ]
+            },
+            {
+                test:/\.(png|svg|jpg|gif)$/,
+                use:['file-loader']
+            },
+            {
+                test:/\.xml$/,
+                use:[
+                    'xml-loader'
+                ]
+            }
+        ]
+    }
+}
